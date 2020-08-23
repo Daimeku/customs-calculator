196a5cf6298d38bbdd40271b1bb1c4cb0acc8090
Daimeku <daimeku@gmail.com>
2020-08-23 16:28:27 -0500

    initial design of UI & setup of project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { TextField, FormControl, InputLabel, Input, InputAdornment, Button } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const CUSTOMS_CATEGORIES = [
+  {
+    "parentCategoryId": "0101",
+    "subCategoryId": "90.10.10",
+    "description": "Race horses, not for breeding",
+    "ratePercentage": 40,
+    "label": "Race heese"
+  },
+  {
+    "parentCategoryId": "0501",
+    "subCategoryId": "00.00.00",
+    "description": "human hair, unworked, whether or not washed or scoured; waste of human hair",
+    "ratePercentage": 5,
+    "label": "human hair"
+  }
+]
+
+class CustomsCalculator extends React.Component {
+
+  render() {
+    return (
+      <div className="calculatorPageContainer">
+
+        <div className="calculatorTitle"> Customs Calculator</div>
+
+        <div className="itemCostControlContainer">
+          <FormControl className="itemCostControl calculatorField" >
+            <InputLabel htmlFor="standard-adornment-amount">Item Cost</InputLabel>
+            <Input
+              id="standard-adornment-amount"
+              name="itemCost"
+              value={''}
+              onChange={() => { }}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            />
+          </FormControl>
+        </div>
+
+
+        <div className="shippingCostControlContainer">
+          <FormControl className={"shippingCostControl calculatorField"}>
+            <InputLabel htmlFor="standard-adornment-amount">Shipping Cost</InputLabel>
+            <Input
+              name="shippingCostInput"
+              value={2000}
+              onChange={() => { }}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            />
+          </FormControl>
+        </div>
+
+        <div className="itemCategoryInputContainer">
+          <Autocomplete
+            name="itemCategoryInput"
+            className="itemCategoryInput calculatorField"
+            options={CUSTOMS_CATEGORIES}
+            getOptionLabel={(category) => category.label}
+            renderInput={(params) => <TextField {...params} label="TextFieldOption" variant="outlined" />}
+          />
+        </div>
+        <div className="calculateButtonContainer">
+          <Button
+            className="calculateButton"
+            variant="contained"
+            color="primary"
+            onClick={() => { }}>
+            Calculate
+          </Button>
+        </div>
+
+
+        <div className="resultArea">
+          <div className="resultsAreaTitle">Results</div>
+          <div className="resultAreaValue">$588282899</div>
+
+          <div className="resultAreaDetails">
+            details go here
+          </div>
+        </div>
+
+      </div>
+
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <CustomsCalculator />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
