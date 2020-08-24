6a32d2e2f5198fd61beb27295bb75f7b03d478bc
Daimeku <daimeku@gmail.com>
2020-08-24 00:05:06 -0500

    updated calculation accuracy, added toggle for calculation details area

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TextField, FormControl, InputAdornment, Button } from '@material-ui/core';
+import { TextField, FormControl, InputAdornment, Button, Switch } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 import './index.css';
@@ -22,9 +22,44 @@ const CUSTOMS_CATEGORIES = [
   }
 ]
 
+const DetailSection = (props) => (
+  <div className="resultAreaDetails">
+    <div className="cifContainer calculationDetailsRow">
+      <p>CIF: </p>
+      <p>{props.calculationDetails.cif}</p>
+    </div>
+
+    <div className="importDutyContainer calculationDetailsRow">
+      <p>Import Duty: </p>
+      <p>{props.calculationDetails.importDuty}</p>
+    </div>
+
+    <div className="gctContainer calculationDetailsRow">
+      <p>GCT:</p>
+      <p>{props.calculationDetails.gct}</p>
+    </div>
+
+    <div className="stampDutyContainer calculationDetailsRow">
+      <p>Stamp Duty:</p>
+      <p>{props.calculationDetails.stampDuty}</p>
+    </div>
+
+    <div className="stampDutyContainer calculationDetailsRow">
+      <p>SCF:</p>
+      <p>{props.calculationDetails.scf}</p>
+    </div>
+
+    <div className="stampDutyContainer calculationDetailsRow">
+      <p>CAF:</p>
+      <p>{props.calculationDetails.caf}</p>
+    </div>
+
+  </div>
+);
+
 class CustomsCalculator extends React.Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       itemCost: {
@@ -42,28 +77,44 @@ class CustomsCalculator extends React.Component {
         error: false,
         errorMessage: ''
       },
-      totalCharges: ''
+      calculationDetails: {
+        importDuty: '',
+        environmentalLevy: '',
+        scf: '',
+        stampDuty: '',
+        caf: '',
+        gct: '',
+        totalCharges: '',
+        cif: ''
+      },
+      showDetails: {
+        value: false
+      }
     }
   }
- 
+
   handleFieldChange = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     let fieldName = event.target.name;
     let fieldObject = this.state[fieldName];
-    let newValue = event.target.value.replace(/\D/g,'');
+    let newValue;
+    if (fieldName !== "showDetails") {
+      newValue = event.target.value.replace(/\D/g, '');
+    } else {
+      newValue = event.target.checked;
+    }
     fieldObject.value = newValue;
     //@Todo - validation
-    
-    this.setState({[fieldName] : fieldObject});
+    this.setState({ [fieldName]: fieldObject });
   }
 
   handleAutocompleteChange = (event, newValue) => {
-    event.preventDefault();
+    // event.preventDefault();
     let itemCategory = this.state.itemCategory;
-    if(newValue === null)
+    if (newValue === null)
       newValue = '';
     itemCategory.value = newValue;
-    this.setState({itemCategory});
+    this.setState({ itemCategory });
   }
 
   resetValidationErrors = () => {
@@ -76,7 +127,7 @@ class CustomsCalculator extends React.Component {
     itemCost.errorMessage = '';
     shippingCost.error = false;
     shippingCost.errorMessage = '';
-    this.setState({itemCategory, itemCost, shippingCost});
+    this.setState({ itemCategory, itemCost, shippingCost });
   }
 
   //use this for the button click
@@ -86,33 +137,33 @@ class CustomsCalculator extends React.Component {
     let itemCategory = this.state.itemCategory;
     let shippingCost = this.state.shippingCost;
     let valid = true;
-    
+
     //check to make sure values are in the fields
-    if(this.state.itemCost.value === ''){
+    if (this.state.itemCost.value === '') {
       valid = false;
       itemCost.error = true;
       itemCost.errorMessage = "Please enter the cost of your item.";
     }
 
-    if(this.state.shippingCost.value === '') {
+    if (this.state.shippingCost.value === '') {
       valid = false;
       shippingCost.error = true;
       shippingCost.errorMessage = "Please enter the cost of shipping your item.";
     }
 
-    if(this.state.itemCategory.value === '') {
+    if (this.state.itemCategory.value === '') {
       valid = false;
       itemCategory.error = true;
       itemCategory.errorMessage = "Please select a category for your item.";
     }
-    if(!valid) {
-      this.setState({itemCategory, itemCost, shippingCost});
+    if (!valid) {
+      this.setState({ itemCategory, itemCost, shippingCost });
       return;
     }
     //call the calculate function
-    let totalCharges = this.calculateTotalShippingCost(itemCost.value, shippingCost.value, itemCategory.value);
+    let calculationDetails = this.calculateTotalShippingCost(itemCost.value, shippingCost.value, itemCategory.value);
 
-    this.setState({totalCharges});
+    this.setState({ calculationDetails });
   }
 
   //use this to do the actual maths
@@ -121,36 +172,45 @@ class CustomsCalculator extends React.Component {
     shippingCost = Number(shippingCost);
 
     //convert to jmd then do this
-    let calculationDetails = {};
-    let insuranceRate = freightType === "air"? 0.01 : 0.015;
+
+    let insuranceRate = freightType === "air" ? 0.01 : 0.015;
     let insuranceCost = insuranceRate * (itemCost + shippingCost);
     let cif = itemCost + shippingCost + insuranceCost;
-    let importDuty = cif * (itemCategory.ratePercentage/100);
+    let importDuty = cif * (itemCategory.ratePercentage / 100);
     let environmentalLevy = cif * 0.005;
     let scf = cif * 0.003;
     let stampDuty = cif > 5500 ? 100 : 5;
     let caf = cif > 5500 ? 2500 : 0;
-    let gct = (cif + caf + environmentalLevy + importDuty + scf) * 0.175;    
-
-    console.log("cif: ", cif);
-    console.log("caf: ", caf);
-    console.log("environmentalLevy: ", environmentalLevy);
-    console.log("gct: ", gct);
-    console.log("import duty: ",importDuty); 
-    console.log("scf: ", scf);
+    let gct = (cif + caf + environmentalLevy + importDuty + scf) * 0.175;
     let totalCharges = importDuty + environmentalLevy + scf + stampDuty + caf + gct;
 
-    return totalCharges;
+    let calculationDetails = {
+      importDuty,
+      environmentalLevy,
+      scf,
+      stampDuty,
+      caf,
+      cif,
+      gct,
+      totalCharges
+    };
+    return calculationDetails;
   }
 
 
   formatCurrency = (number) => {
-    number = number.replace(/\D/g,'');
+    number = number.replace(/\D/g, '');
     number = Number(number);
-    if(number === 0)
+    if (number === 0)
       return '';
     let numberFormat = new Intl.NumberFormat("EN-US");
     return numberFormat.format(number);
+  }
+
+  getAdornment = () => {
+    return {
+      startAdornment: <InputAdornment position="start">$</InputAdornment>
+    }
   }
 
   render() {
@@ -167,9 +227,7 @@ class CustomsCalculator extends React.Component {
               label="Item Cost"
               value={this.formatCurrency(this.state.itemCost.value)}
               onChange={this.handleFieldChange}
-              InputProps={{
-                startAdornment: <InputAdornment position="start">$</InputAdornment>
-              }}
+              InputProps={this.state.itemCost.value === '' ? null : this.getAdornment()}
               error={this.state.itemCost.error}
               helperText={this.state.itemCost.errorMessage}
             />
@@ -184,9 +242,7 @@ class CustomsCalculator extends React.Component {
               label="Shipping Cost"
               value={this.formatCurrency(this.state.shippingCost.value)}
               onChange={this.handleFieldChange}
-              InputProps={{
-                startAdornment: <InputAdornment position="start">$</InputAdornment>
-              }}
+              InputProps={this.state.shippingCost.value === '' ? null : this.getAdornment()}
               error={this.state.shippingCost.error}
               helperText={this.state.shippingCost.errorMessage}
             />
@@ -202,7 +258,7 @@ class CustomsCalculator extends React.Component {
             renderInput={(params) => <TextField {...params}
               error={this.state.itemCategory.error}
               helperText={this.state.itemCategory.errorMessage}
-              label="Item Category" 
+              label="Item Category"
               variant="outlined"
             />
             }
@@ -222,12 +278,24 @@ class CustomsCalculator extends React.Component {
 
 
         <div className="resultArea">
-          <div className="resultsAreaTitle">Results</div>
-          <div className="resultAreaValue">{this.state.totalCharges}</div>
+          <div className="resultsAreaTitle">Total Import Charges</div>
+          <div className="resultAreaValue">{this.state.calculationDetails.totalCharges}</div>
 
-          <div className="resultAreaDetails">
-            details go here
+          <div className="resultAreaDetailsSwitch">
+          <p>show details</p>
+            <Switch
+              checked={this.state.showDetails.value}
+              onChange={this.handleFieldChange}
+              color="primary"
+              name="showDetails"
+            />
           </div>
+          {
+            this.state.showDetails.value ?
+            <DetailSection calculationDetails={this.state.calculationDetails}/> :
+            null
+          }
+
         </div>
 
       </div>
